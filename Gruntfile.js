1dee35df9855958b3195a7b167a8967f8fcb17e9
chris <christopherjoshuasoares>
2024-12-05 17:46:12 -0300

    upgrade

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,8 +58,8 @@ module.exports = function (grunt) {
             },
             {
               match: "ENDERECO_DO_JS",
-              replacement: "./src/scripts/main.min.js"
-            }
+              replacement: "./scripts/main.min.js" // Corrigido o caminho
+          }
           ]
         },
         files: [
